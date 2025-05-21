055348782bc0dde7c61b580732abd17e3d76d747
Iman Afshar <iman.afshar2023@gmail.com>
2025-05-21 12:50:24 +0330

    make images directory and read image in main.py

--- main.py
+++ main.py
@@ -2,5 +2,13 @@ import cv2
 import numpy as np
 import os
 
+def main(image_path, output_dir="cells"):
+    if not os.path.exists(output_dir):
+        os.makedirs(output_dir)
+
+    img = cv2.imread(image_path)
+    
 
 print("sudoku solver with opencv :)")
+img_path = "images/sudoku.jpg"
+main(img_path)
