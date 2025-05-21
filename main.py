4f7a3a8f71915f9932683a307a3467e76eb650ac
Iman Afshar <iman.afshar2023@gmail.com>
2025-05-21 13:55:55 +0330

    Accounting dimensions table

--- main.py
+++ main.py
@@ -43,10 +43,30 @@ def main(image_path, output_dir="cells"):
         print("خطا: هیچ چهارضلعی‌ای پیدا نشد.")
         return False
     
-    img_contour = img.copy()
-    cv2.drawContours(img_contour, [sudoku_contour], 0, (0, 0, 255), 3)
-    cv2.imshow("Detected Sudoku Contour", img_contour)
-    cv2.waitKey(0)
+    # img_contour = img.copy()
+    # cv2.drawContours(img_contour, [sudoku_contour], 0, (0, 0, 255), 3)
+    # cv2.imshow("Detected Sudoku Contour", img_contour)
+    # cv2.waitKey(0)
+
+    points = sudoku_contour.reshape(4, 2)
+    rect = np.zeros((4, 2), dtype="float32")
+    s = points.sum(axis=1)
+    rect[0] = points[np.argmin(s)]  # بالا-چپ
+    rect[2] = points[np.argmax(s)]  # پایین-راست
+    diff = np.diff(points, axis=1)
+    rect[1] = points[np.argmin(diff)]  # بالا-راست
+    rect[3] = points[np.argmax(diff)]  # پایین-چپ
+    
+    # محاسبه ابعاد جدول
+    (tl, tr, br, bl) = rect
+    width_a = np.sqrt(((br[0] - bl[0]) ** 2) + ((br[1] - bl[1]) ** 2))
+    width_b = np.sqrt(((tr[0] - tl[0]) ** 2) + ((tr[1] - tl[1]) ** 2))
+    max_width = max(int(width_a), int(width_b))
+    height_a = np.sqrt(((tr[0] - br[0]) ** 2) + ((tr[1] - br[1]) ** 2))
+    height_b = np.sqrt(((tl[0] - bl[0]) ** 2) + ((tl[1] - bl[1]) ** 2))
+    max_height = max(int(height_a), int(height_b))
+    
+    
 
 
 print("sudoku solver with opencv :)")
